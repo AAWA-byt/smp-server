e40fa6e7dc44d091bb383874da7ed77fed00796c
AAWA-byt <mail@aaron.is>
2021-12-01 18:12:47 +0100

    :sparkles: created team menu manager

---
+++ src/main/java/online/minipixel/smp/Inventorys/Vip.java
@@ -0,0 +1,12 @@
+package online.minipixel.smp.Inventorys;
+
+public class Vip {
+
+    public static void open_vip_menu() {
+    create_vip_menu();
+}
+
+        private static void create_vip_menu() {
+
+        }
+}
