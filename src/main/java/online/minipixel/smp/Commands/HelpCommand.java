3300f2f52c58c4354ef2913ec1db9e4422b204c4
AAWA-byt <mail@aaron.is>
2021-12-01 17:50:32 +0100

    :saprkles: created HelpCommand

---
+++ src/main/java/online/minipixel/smp/Commands/HelpCommand.java
@@ -0,0 +1,14 @@
+package online.minipixel.smp.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class HelpCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        return false;
+    }
+}
