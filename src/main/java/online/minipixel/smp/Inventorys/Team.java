06e840e68b81836d193174fad15dadb4b7674056
AAWA-byt <mail@aaron.is>
2021-12-01 18:10:09 +0100

    :sparkles: created team menu manager

---
+++ src/main/java/online/minipixel/smp/Inventorys/Team.java
@@ -0,0 +1,12 @@
+package online.minipixel.smp.Inventorys;
+
+public class Team {
+
+    public static void open_team_menu() {
+        create_team_menu();
+    }
+
+    private static void create_team_menu() {
+
+    }
+}
