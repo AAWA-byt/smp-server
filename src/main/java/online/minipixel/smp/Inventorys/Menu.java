e016cf1dcf6066dbcc5c79c52a018c25421e7e07
AAWA-byt <mail@aaron.is>
2021-12-02 09:58:26 +0100

    :zap: added three items to menu inventory

--- src/main/java/online/minipixel/smp/Inventorys/Menu.java
+++ src/main/java/online/minipixel/smp/Inventorys/Menu.java
@@ -1,5 +1,10 @@
 package online.minipixel.smp.Inventorys;
 
+import online.minipixel.smp.Main;
+import online.minipixel.smp.Utils.ItemBuilder;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemFlag;
+
 public class Menu {
 
     public static void open_main_menu() {
@@ -8,5 +13,13 @@ public class Menu {
 
     private static void create_main_menu() {
 
+        Main.getMenu().setItem(0, new ItemBuilder(Material.COMPASS, 1).setDisplayname("")
+                .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).build());
+
+        Main.getMenu().setItem(1, new ItemBuilder(Material.CHEST, 1).setDisplayname("")
+                .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).build());
+
+        Main.getMenu().setItem(2, new ItemBuilder(Material.SLIME_BALL, 1).setDisplayname("")
+                .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).build());
     }
 }
