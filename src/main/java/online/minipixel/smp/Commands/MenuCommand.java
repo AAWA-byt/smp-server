62cc147c1b82abdab1504787df8f220e44527847
AAWA-byt <mail@aaron.is>
2021-12-06 21:56:23 +0100

    :zap: updated menu command

--- src/main/java/online/minipixel/smp/Commands/MenuCommand.java
+++ src/main/java/online/minipixel/smp/Commands/MenuCommand.java
@@ -1,13 +1,24 @@
 package online.minipixel.smp.Commands;
 
+import online.minipixel.smp.Main;
+import online.minipixel.smp.MessageAPI.Prefix;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.jetbrains.annotations.NotNull;
+import org.bukkit.entity.Player;
 
 public class MenuCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(Prefix.System_Prefix + "§cDu bist kein Spieler!");
+            return true;
+        }
+
+        Player player = (Player) sender;
+        player.openInventory(Main.getMenu());
+
         return false;
     }
 }
