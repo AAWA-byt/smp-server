e7cb90733c90b859f8e5bfa600d55bf421ec7498
AAWA-byt <mail@aaron.is>
2021-12-06 14:24:28 +0100

    :sparkles: created JoinMethods and :zap: updated JoinListener

--- src/main/java/online/minipixel/smp/Listener/JoinListener.java
+++ src/main/java/online/minipixel/smp/Listener/JoinListener.java
@@ -1,6 +1,22 @@
 package online.minipixel.smp.Listener;
 
+import online.minipixel.smp.MessageAPI.JoinMethod;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class JoinListener implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        Player player = e.getPlayer();
+        new JoinMethod().defaultJoin(player);
+    }
+
+    @EventHandler
+    public void onFirstJoin(PlayerJoinEvent e) {
+        Player player = e.getPlayer();
+        new JoinMethod().firstJoin(player);
+    }
 }
---
+++ src/main/java/online/minipixel/smp/MessageAPI/JoinMethod.java
@@ -0,0 +1,16 @@
+package online.minipixel.smp.MessageAPI;
+
+import org.bukkit.entity.Player;
+
+public class JoinMethod {
+
+    public void firstJoin(Player player) {
+
+
+    }
+
+    public void defaultJoin(Player player) {
+
+
+    }
+}
