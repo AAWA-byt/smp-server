5db128f4d04f65c376886715bbf612a23e38c757
AAWA-byt <mail@aaron.is>
2021-12-07 11:20:40 +0100

    :sparkles: created BackpackCommand

---
+++ src/main/java/online/minipixel/smp/Commands/BackpackCommand.java
@@ -0,0 +1,13 @@
+package online.minipixel.smp.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class BackpackCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        return false;
+    }
+}
