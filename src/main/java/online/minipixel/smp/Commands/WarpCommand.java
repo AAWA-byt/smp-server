2a268c410f9032409a2cd55320eaca1ab6496c44
AAWA-byt <mail@aaron.is>
2021-12-09 22:44:35 +0100

    :sparkles: created WarpCommand

---
+++ src/main/java/online/minipixel/smp/Commands/WarpCommand.java
@@ -0,0 +1,14 @@
+package online.minipixel.smp.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class WarpCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+
+        return false;
+    }
+}
