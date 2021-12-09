3b251d40065deb1292c720aad0af9654de07c2cc
AAWA-byt <mail@aaron.is>
2021-12-09 12:45:28 +0100

    :sparkles: created ClickListener

---
+++ src/main/java/online/minipixel/smp/Listener/ClickListener.java
@@ -0,0 +1,13 @@
+package online.minipixel.smp.Listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class ClickListener implements Listener {
+
+    @EventHandler
+    public void onClick(InventoryClickEvent e) {
+
+    }
+}
