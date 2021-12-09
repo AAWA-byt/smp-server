84811e4a520d2b5d794b0b1f16a47843938972f1
AAWA-byt <mail@aaron.is>
2021-12-09 11:52:25 +0100

    :zap: added three new items to vip menu inventory

--- src/main/java/online/minipixel/smp/Inventorys/Vip.java
+++ src/main/java/online/minipixel/smp/Inventorys/Vip.java
@@ -1,12 +1,28 @@
 package online.minipixel.smp.Inventorys;
 
+import online.minipixel.smp.Main;
+import online.minipixel.smp.Utils.ItemBuilder;
+import org.bukkit.Material;
+import org.bukkit.entity.Item;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+
 public class Vip {
 
     public static void open_vip_menu() {
+
     create_vip_menu();
 }
 
         private static void create_vip_menu() {
 
+            Main.getVip().setItem(0, new ItemBuilder(Material.COMPASS, 1).setDisplayname("§9§lTeleporter §7(Linksklick)")
+                    .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).build());
+
+            Main.getVip().setItem(1, new ItemBuilder(Material.PLAYER_HEAD, 1).setDisplayname("§2§lSkins §7(Linksklick)")
+                    .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).build());
+
+            Main.getVip().setItem(2, new ItemBuilder(Material.GLOWSTONE_DUST,1).setDisplayname("§e§lPartikel §7(Linksklick)")
+                    .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).build());
         }
 }
