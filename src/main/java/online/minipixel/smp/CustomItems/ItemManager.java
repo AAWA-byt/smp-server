15401741cc0f6f11e5e473bf98c4a6d07ad63a34
AAWA-byt <mail@aaron.is>
2021-12-10 16:12:06 +0100

    :sparkles: created ItemManager and registered items

---
+++ src/main/java/online/minipixel/smp/CustomItems/ItemManager.java
@@ -0,0 +1,20 @@
+package online.minipixel.smp.CustomItems;
+
+import online.minipixel.smp.CustomItems.Items.*;
+
+public class ItemManager {
+
+    public static void initItems() {
+
+        EmeraldHelmet.createEmeraldHelmet();
+        EmeraldChestplatte.getCustomEmeraldChestplate();
+        EmeraldLeggings.getCustomEmeraldleggings();
+        EmeraldBoots.getCustomEmeraldBoots();
+
+        EmeraldSword.getCustomEmeraldsword();
+        ObsidianSword.getCustomObsidianSword();
+        Lapispickaxe.getCustomLapisPickAxe();
+        Redstoneboots.getCustomRedStoneBoots();
+
+    }
+}
