c255a0da2ca23f8e42a5d954f8cb833a7f1dc0e6
AAWA-byt <mail@aaron.is>
2021-12-10 13:09:12 +0100

    :sparkles: created DiscordBotCommand

---
+++ src/main/java/online/minipixel/smp/Commands/DiscordBotCommand.java
@@ -0,0 +1,13 @@
+package online.minipixel.smp.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class DiscordBotCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return false;
+    }
+}
