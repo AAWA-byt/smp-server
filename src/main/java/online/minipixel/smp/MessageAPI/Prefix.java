e63644dbf0fbcf857d99c1f1fe45449512f4c97e
AAWA-byt <mail@aaron.is>
2021-12-10 12:41:59 +0100

    :sparkles: created WarpSystem Prefix

--- src/main/java/online/minipixel/smp/MessageAPI/Prefix.java
+++ src/main/java/online/minipixel/smp/MessageAPI/Prefix.java
@@ -2,5 +2,6 @@ package online.minipixel.smp.MessageAPI;
 
 public class Prefix {
 
-    public static String System_Prefix = "";
+    public static String System_Prefix = "§4System §8§l>> §f";
+    public static String WarpSystem_Prefix = "§2WarpSystem §8§l>> §f";
 }
