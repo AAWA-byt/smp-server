02aca87722ff38bec716ae25e6fcafce0349023b
AAWA-byt <mail@aaron.is>
2021-12-12 00:59:06 +0100

    :fire: removed @NotNull

--- src/main/java/online/minipixel/smp/Commands/DiscordBotCommand.java
+++ src/main/java/online/minipixel/smp/Commands/DiscordBotCommand.java
@@ -3,11 +3,11 @@ package online.minipixel.smp.Commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.jetbrains.annotations.NotNull;
+
 
 public class DiscordBotCommand implements CommandExecutor {
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         return false;
     }
 }
