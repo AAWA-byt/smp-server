da94c58eb4d6afb982bacf7b09c5209a4c4fa060
AAWA-byt <mail@aaron.is>
2021-12-12 16:33:52 +0100

    :zap: updated ClickListener

--- src/main/java/online/minipixel/smp/Listener/ClickListener.java
+++ src/main/java/online/minipixel/smp/Listener/ClickListener.java
@@ -1,13 +1,22 @@
 package online.minipixel.smp.Listener;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 
 public class ClickListener implements Listener {
 
-    @EventHandler
-    public void onClick(InventoryClickEvent e) {
 
+    @EventHandler
+    public void OnClickTeleporter(InventoryClickEvent event) {
+        if (event.getView().getTitle().equals("§8§lTeleporter")) {
+            event.setCancelled(true);
+            if (event.getCurrentItem() != null) {
+                String playername = event.getCurrentItem().getItemMeta().getLocalizedName();
+                event.getWhoClicked().teleport(Bukkit.getPlayer(playername));
+
+            }
+        }
     }
 }
