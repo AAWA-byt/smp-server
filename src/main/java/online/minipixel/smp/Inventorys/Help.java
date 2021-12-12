7c8e883f7766ffbc14db7eaea05ffd3f294a7dba
AAWA-byt <mail@aaron.is>
2021-12-12 01:02:42 +0100

    :zap: added new item to help menu

--- src/main/java/online/minipixel/smp/Inventorys/Help.java
+++ src/main/java/online/minipixel/smp/Inventorys/Help.java
@@ -1,5 +1,10 @@
 package online.minipixel.smp.Inventorys;
 
+import online.minipixel.smp.Main;
+import online.minipixel.smp.Utils.ItemBuilder;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemFlag;
+
 public class Help {
 
     public static void open_help_menu() {
@@ -7,6 +12,9 @@ public class Help {
     }
 
     private static void create_help_menu() {
+
+        Main.getHelp().setItem(0, new ItemBuilder(Material.BOOK, 1).setDisplayname("")
+                .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).build());
 
     }
 }
