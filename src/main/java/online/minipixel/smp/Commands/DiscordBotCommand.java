0abc509f7ed69eb62a0541e2aa3d7056b6910290
AAWA-byt <mail@aaron.is>
2021-12-12 01:01:15 +0100

    :zap: added Builder to DiscordBotCommand.java

--- src/main/java/online/minipixel/smp/Commands/DiscordBotCommand.java
+++ src/main/java/online/minipixel/smp/Commands/DiscordBotCommand.java
@@ -1,13 +1,22 @@
 package online.minipixel.smp.Commands;
 
+import online.minipixel.smp.DiscordBot.Builder;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+
+import javax.security.auth.login.LoginException;
 
 
 public class DiscordBotCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        try {
+            new Builder().create();
+        } catch (LoginException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 }
