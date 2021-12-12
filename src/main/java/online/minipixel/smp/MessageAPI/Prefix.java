d5d0c40dd2dce048648d3e3d5d4e5f9c643ba01c
AAWA-byt <mail@aaron.is>
2021-12-12 01:16:15 +0100

    :zap: added plugin prefix

--- src/main/java/online/minipixel/smp/MessageAPI/Prefix.java
+++ src/main/java/online/minipixel/smp/MessageAPI/Prefix.java
@@ -5,4 +5,5 @@ public class Prefix {
     public static String System_Prefix = "§4System §8§l>> §f";
     public static String WarpSystem_Prefix = "§2WarpSystem §8§l>> §f";
     public static String DiscordBot_Prefix = "§9Discord §8§l>> §f";
+    public static String Plugin_Prefix = "§aSMP §8§l>> §f";
 }
