6172beb983a6b6bcf917904b05ada7b142fa1881
AAWA-byt <mail@aaron.is>
2021-12-21 14:48:56 +0100

    :zap: player will now receive a message with their death cords

--- src/main/java/online/minipixel/smp/Listener/DeathListener.java
+++ src/main/java/online/minipixel/smp/Listener/DeathListener.java
@@ -1,6 +1,18 @@
 package online.minipixel.smp.Listener;
 
+import online.minipixel.smp.MessageAPI.Prefix;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class DeathListener implements Listener {
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent e) {
+        Player player = e.getPlayer();
+
+        player.sendMessage(Prefix.Plugin_Prefix + "§7Du bist bei §a" + player.getLocation().getBlockX() + " " + player.getLocation().getBlockY() + " " +
+                player.getLocation().getBlockZ() + " §7gestorben.");
+    }
 }
