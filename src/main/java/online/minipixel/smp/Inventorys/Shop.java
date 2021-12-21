98dd3f8bf1a2481ccaee0379c66aa9af7a3e1912
AAWA-byt <mail@aaron.is>
2021-12-21 15:00:40 +0100

    :sparkles: created shop inventory manager

---
+++ src/main/java/online/minipixel/smp/Inventorys/Shop.java
@@ -0,0 +1,12 @@
+package online.minipixel.smp.Inventorys;
+
+public class Shop {
+
+    public static void open_shop_menu() {
+        create_shop_menu();
+    }
+
+    private static  void create_shop_menu() {
+
+    }
+}
