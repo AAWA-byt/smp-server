92232398e245ab2b520f6dbc6c51380aab335774
AAWA-byt <mail@aaron.is>
2021-12-22 16:45:36 +0100

    :zap: added public void equip method

--- src/main/java/online/minipixel/smp/Utils/JoinMethod.java
+++ src/main/java/online/minipixel/smp/Utils/JoinMethod.java
@@ -1,6 +1,8 @@
 package online.minipixel.smp.Utils;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 
 public class JoinMethod {
 
@@ -11,5 +13,15 @@ public class JoinMethod {
     public void defaultJoin(Player player) {
 
 
+    }
+
+    public void equip(Player player) {
+
+        Inventory inventory = player.getInventory();
+
+        inventory.addItem(new ItemBuilder(Material.STONE_SWORD, 1).build());
+        inventory.addItem(new ItemBuilder(Material.STONE_AXE, 1).build());
+        inventory.addItem(new ItemBuilder(Material.STONE_PICKAXE, 1).build());
+        inventory.addItem(new ItemBuilder(Material.BREAD, 20).build());
     }
 }
