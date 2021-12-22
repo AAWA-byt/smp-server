03bbb22204dabf40dffce9008721a4c7beffda2f
AAWA-byt <mail@aaron.is>
2021-12-22 16:21:04 +0100

    :zap: updated JoinListener.java

--- src/main/java/online/minipixel/smp/Listener/JoinListener.java
+++ src/main/java/online/minipixel/smp/Listener/JoinListener.java
@@ -2,7 +2,9 @@ package online.minipixel.smp.Listener;
 
 import online.minipixel.smp.Main;
 import online.minipixel.smp.MessageAPI.JoinMethod;
+import online.minipixel.smp.MessageAPI.Prefix;
 import online.minipixel.smp.Scoreboard.DefaultScoreboard;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,11 +21,20 @@ public class JoinListener implements Listener {
         Main.getINSTANCE().getTablistManager().setTablist(player);
         Main.getINSTANCE().getTablistManager().setPlayerTeam(player);
         Main.getINSTANCE().getTablistManager().setAllPlayerTeams();
+
+        e.setJoinMessage("§a>> §7" + player.getName());
     }
 
     @EventHandler
     public void onFirstJoin(PlayerJoinEvent e) {
         Player player = e.getPlayer();
-        new JoinMethod().firstJoin(player);
+
+        if (!player.hasPlayedBefore()) {
+            new JoinMethod().firstJoin(player);
+            e.setJoinMessage(null);
+            Bukkit.getOnlinePlayers().forEach(player1 -> {
+                player1.sendMessage(Prefix.Plugin_Prefix + "§7Willkommen auf §4minipixel.online §7" + e.getPlayer().getName() + ".");
+            });
+        }
     }
 }
