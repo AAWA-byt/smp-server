6c5ec0a51b66bcfb2e5dde6c926089ea4820250b
AAWA-byt <mail@aaron.is>
2021-12-27 12:49:33 +0100

    :zap: added method for ParticelInventory and added the first 4 items

--- src/main/java/online/minipixel/smp/ModelsParticel/ParticelInventory.java
+++ src/main/java/online/minipixel/smp/ModelsParticel/ParticelInventory.java
@@ -1,4 +1,35 @@
 package online.minipixel.smp.ModelsParticel;
 
+import online.minipixel.smp.Utils.ItemBuilder;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
+
 public class ParticelInventory {
+
+    Inventory particels;
+
+    public void open(Player player) {
+
+        if (player.hasPermission("minipixel.vip")) {
+
+
+            particels = Bukkit.createInventory(player, 9*1, "§8Partikel Menü");
+
+            particels.setItem(0, new ItemBuilder(Material.TOTEM_OF_UNDYING, 1).setDisplayname("§6§lTotem Effekt §7(Linksklick)")
+                    .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).build());
+
+            particels.setItem(1, new ItemBuilder(Material.DRAGON_EGG, 1).setDisplayname("§5§lDrachen Effekt §7(Linksklick)")
+                    .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).build());
+
+            particels.setItem(2, new ItemBuilder(Material.WHITE_WOOL, 1).setDisplayname("§f§lWolken Effekt §7(Linksklick)")
+                    .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).build());
+
+            particels.setItem(8, new ItemBuilder(Material.BARRIER, 1).setDisplayname("§c§lEffekte entfernen §7(Linksklick)")
+                    .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).build());
+
+        }
+    }
 }
