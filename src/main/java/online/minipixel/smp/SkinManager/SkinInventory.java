541fc537da65ed302d684f6023b50a720b82b02f
AAWA-byt <mail@aaron.is>
2022-01-03 17:35:04 +0100

    :zap: added methods for creating inventory for choosing a skin

--- src/main/java/online/minipixel/smp/SkinManager/SkinInventory.java
+++ src/main/java/online/minipixel/smp/SkinManager/SkinInventory.java
@@ -1,5 +1,20 @@
 package online.minipixel.smp.SkinManager;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+
 public class SkinInventory {
+
+    public void open(Player player) {
+
+        Inventory skins;
+        skins = Bukkit.createInventory(player, 9*1, "§8§lWähle einen Skin...");
+        //TODO: add items for skins
+
+        player.openInventory(skins);
+
+    }
 
 }
