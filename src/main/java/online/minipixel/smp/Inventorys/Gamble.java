9db99d81659750ba250c144f35e643f36f72d55b
AAWA-byt <mail@aaron.is>
2022-01-04 17:07:07 +0100

    :zap: added inventory manager

--- src/main/java/online/minipixel/smp/Inventorys/Gamble.java
+++ src/main/java/online/minipixel/smp/Inventorys/Gamble.java
@@ -2,5 +2,11 @@ package online.minipixel.smp.Inventorys;
 
 public class Gamble {
 
+    public static void open_gamble_menu() {
+        create_gamble_menu();
+    }
 
+    private static void create_gamble_menu() {
+
+    }
 }
