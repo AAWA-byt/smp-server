c3bbbfc373070a56634be2c4da6b30dbedf0075a
AAWA-byt <mail@aaron.is>
2022-01-04 01:12:18 +0100

    :zap: added code for BackpackCommand

--- src/main/java/online/minipixel/smp/Commands/BackpackCommand.java
+++ src/main/java/online/minipixel/smp/Commands/BackpackCommand.java
@@ -1,13 +1,27 @@
 package online.minipixel.smp.Commands;
 
+import online.minipixel.smp.Backpack.Backpack;
+import online.minipixel.smp.Main;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
-public class BackpackCommand implements CommandExecutor {
+public class BackpackCommand  implements CommandExecutor {
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        return false;
+        if(!(sender instanceof Player)) {
+            return true;
+        }
+
+        Player player = (Player) sender;
+
+        Backpack backpack = Main.getINSTANCE().getBackpackManager().getBackpack(player.getUniqueId());
+
+        player.openInventory(backpack.getInventory());
+        return true;
     }
+
 }
