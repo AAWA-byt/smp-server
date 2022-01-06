fc3958c8f36a2e287e7fe04a6b2cd814fe07b6a1
AAWA-byt <mail@aaron.is>
2022-01-07 00:17:51 +0100

    :zap: set quit message to null

--- src/main/java/online/minipixel/smp/Listener/QuitListener.java
+++ src/main/java/online/minipixel/smp/Listener/QuitListener.java
@@ -1,6 +1,13 @@
 package online.minipixel.smp.Listener;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class QuitListener implements Listener {
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent e) {
+        e.setQuitMessage(null);
+    }
 }
