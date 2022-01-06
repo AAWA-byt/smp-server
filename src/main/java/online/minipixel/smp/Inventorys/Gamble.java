5b117fcc19d262c76d6d79077360b5cc1fba9945
AAWA-byt <mail@aaron.is>
2022-01-06 16:42:14 +0100

    :art: changed space between items

--- src/main/java/online/minipixel/smp/Inventorys/Gamble.java
+++ src/main/java/online/minipixel/smp/Inventorys/Gamble.java
@@ -15,16 +15,22 @@ public class Gamble {
 
         Main.getGamble().setItem(10, new ItemBuilder(Material.DIAMOND, 1).setDisplayname("§6§lLotterie 1 §7(Linksklick)")
                 .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).setLore("", "§7Preis: 1x Diamant", "").setLocalizedName("l1").build());
+
         Main.getGamble().setItem(11, new ItemBuilder(Material.DIAMOND, 1).setDisplayname("§6§lLotterie 2 §7(Linksklick)")
                 .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).setLore("", "§7Preis: 4x Diamant", "").setLocalizedName("l2").build());
+
         Main.getGamble().setItem(12, new ItemBuilder(Material.DIAMOND, 1).setDisplayname("§6§lLotterie 3 §7(Linksklick)")
                 .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).setLore("", "§7Preis: 6x Diamant", "").setLocalizedName("l3").build());
+
         Main.getGamble().setItem(13, new ItemBuilder(Material.BOOK, 1).setDisplayname("§9§lAnleitung")
                 .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).build());
+
         Main.getGamble().setItem(14, new ItemBuilder(Material.DIAMOND, 1).setDisplayname("§6§lLotterie 4 §7(Linksklick)")
                 .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).setLore("", "§7Preis: 8x Diamant", "").setLocalizedName("l4").build());
+
         Main.getGamble().setItem(15, new ItemBuilder(Material.DIAMOND, 1).setDisplayname("§6§lLotterie 5 §7(Linksklick)")
                 .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).setLore("", "§7Preis: 10x Diamant", "").setLocalizedName("l5").build());
+
         Main.getGamble().setItem(16, new ItemBuilder(Material.DIAMOND, 1).setDisplayname("§6§lLotterie 6 §7(Linksklick)")
                 .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).setLore("", "§7Preis: 15x Diamant", "").setLocalizedName("l6").build());
 
