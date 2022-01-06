31436258c773e1161debd9b1ad1ab884ff3b9e8a
AAWA-byt <mail@aaron.is>
2022-01-06 16:34:57 +0100

    :zap: added gamble items with localized names to inventory click listener

--- src/main/java/online/minipixel/smp/Listener/ClickListener.java
+++ src/main/java/online/minipixel/smp/Listener/ClickListener.java
@@ -1,5 +1,6 @@
 package online.minipixel.smp.Listener;
 
+import online.minipixel.smp.GambleSystem.gambles.*;
 import online.minipixel.smp.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
@@ -74,6 +75,52 @@ public class ClickListener implements Listener {
                     player.closeInventory();
                     player.updateInventory();
 
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onClickGamble(InventoryClickEvent e) {
+        Player player = (Player) e.getWhoClicked();
+
+        if (e.getView().getTitle().equals("§6§lLotterie")) {
+            e.setCancelled(true);
+            if (e.getCurrentItem() != null) {
+
+                if (e.getCurrentItem().getItemMeta().hasLocalizedName()) {
+
+                    if (e.getCurrentItem().getItemMeta().getLocalizedName() == "l1") {
+                        player.getInventory().close();
+                        player.updateInventory();
+                        new Gamble_1().gamble(player);
+
+                    } else if (e.getCurrentItem().getItemMeta().getLocalizedName() == "l2") {
+                        player.getInventory().close();
+                        player.updateInventory();
+                        new Gamble_2().gamble(player);
+
+                    } else if (e.getCurrentItem().getItemMeta().getLocalizedName() == "l3") {
+                        player.getInventory().close();
+                        player.updateInventory();
+                        new Gamble_3().gamble(player);
+
+                    } else if (e.getCurrentItem().getItemMeta().getLocalizedName() == "l4") {
+                        player.getInventory().close();
+                        player.updateInventory();
+                        new Gamble_4().gamble(player);
+
+                    } else if (e.getCurrentItem().getItemMeta().getLocalizedName() == "l5") {
+                        player.getInventory().close();
+                        player.updateInventory();
+                        new Gamble_5().gamble(player);
+
+                    } else if (e.getCurrentItem().getItemMeta().getLocalizedName() == "l6") {
+                        player.getInventory().close();
+                        player.updateInventory();
+                        new Gamble_6().gamble(player);
+
+                    }
                 }
             }
         }
