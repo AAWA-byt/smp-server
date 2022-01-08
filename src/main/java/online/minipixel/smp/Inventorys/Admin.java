b94222fe13222c1c606bd9ac9a7bddc92d2cb3a8
AAWA-byt <mail@aaron.is>
2022-01-08 20:22:08 +0100

    :zap: added manager methods for inventory

--- src/main/java/online/minipixel/smp/Inventorys/Admin.java
+++ src/main/java/online/minipixel/smp/Inventorys/Admin.java
@@ -1,4 +1,12 @@
 package online.minipixel.smp.Inventorys;
 
 public class Admin {
+
+    public static void open_admin_menu() {
+        create_admin_menu();
+    }
+
+    private static void create_admin_menu() {
+
+    }
 }
