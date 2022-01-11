39bebe10db5bc5140625e97215ced7d71ba1e117
AAWA-byt <mail@aaron.is>
2022-01-11 23:05:12 +0100

    :zap: added new item to vip menu

--- src/main/java/online/minipixel/smp/Inventorys/Vip.java
+++ src/main/java/online/minipixel/smp/Inventorys/Vip.java
@@ -22,5 +22,8 @@ public class Vip {
 
             Main.getVip().setItem(3, new ItemBuilder(Material.DRAGON_EGG,1).setDisplayname("§b§lDrachen Effekt §7(Linksklick)")
                     .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).build());
+
+            Main.getVip().setItem(8, new ItemBuilder(Material.BARRIER, 1).setDisplayname("§c§lEffekte entfernen §7(Linksklick)")
+                    .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).build());
         }
 }
