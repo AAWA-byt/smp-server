94cab3eb54a948b889372e654a76caa50a77c217
AAWA-byt <mail@aaron.is>
2022-01-12 09:42:50 +0100

    :zap: finished issue #10

--- src/main/java/online/minipixel/smp/Commands/VipCommand.java
+++ src/main/java/online/minipixel/smp/Commands/VipCommand.java
@@ -1,12 +1,22 @@
 package online.minipixel.smp.Commands;
 
+import online.minipixel.smp.Main;
+import online.minipixel.smp.MessageAPI.Prefix;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class VipCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(Prefix.System_Prefix + "§cDu bist kein Spieler!");
+        }
+
+        Player player = (Player) sender;
+        player.openInventory(Main.getVip());
         return false;
     }
 }
