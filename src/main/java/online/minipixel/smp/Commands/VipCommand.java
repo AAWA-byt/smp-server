0ea0dd78c08a1aff1c03bc82f3feac3e696c2cf1
AAWA-byt <mail@aaron.is>
2022-01-12 09:40:14 +0100

    :fire: removed @NotNull from JetBrains

--- src/main/java/online/minipixel/smp/Commands/VipCommand.java
+++ src/main/java/online/minipixel/smp/Commands/VipCommand.java
@@ -3,7 +3,6 @@ package online.minipixel.smp.Commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.jetbrains.annotations.NotNull;
 
 public class VipCommand implements CommandExecutor {
     @Override
