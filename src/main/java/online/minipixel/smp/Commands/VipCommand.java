c06d4bf90bca74a71e5b0fac143cc1b9603cf16b
AAWA-byt <mail@aaron.is>
2022-01-12 09:38:07 +0100

    :sparkles: created VipCommand.java

---
+++ src/main/java/online/minipixel/smp/Commands/VipCommand.java
@@ -0,0 +1,13 @@
+package online.minipixel.smp.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class VipCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+}
