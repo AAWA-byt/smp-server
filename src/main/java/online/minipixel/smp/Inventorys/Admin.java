570c4181c43a1beb079d98ec4bb72bc35666410b
AAWA-byt <mail@aaron.is>
2022-01-13 11:52:11 +0100

    :zap: added item for closing the iventory

--- src/main/java/online/minipixel/smp/Inventorys/Admin.java
+++ src/main/java/online/minipixel/smp/Inventorys/Admin.java
@@ -1,5 +1,10 @@
 package online.minipixel.smp.Inventorys;
 
+import online.minipixel.smp.Main;
+import online.minipixel.smp.Utils.ItemBuilder;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemFlag;
+
 public class Admin {
 
     public static void open_admin_menu() {
@@ -8,5 +13,7 @@ public class Admin {
 
     private static void create_admin_menu() {
 
+        Main.getAdmin().setItem(8, new ItemBuilder(Material.BARRIER, 1).setDisplayname("§c§lSchließen §7(Linksklick)")
+                .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).build());
     }
 }
