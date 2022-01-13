317ce9f91629c4209b53329b0ca1f41d3c471d17
AAWA-byt <mail@aaron.is>
2022-01-13 14:25:21 +0100

    :boom: added gamble system for Gamble_Nether

--- src/main/java/online/minipixel/smp/GambleSystem/Gamble_Nether.java
+++ src/main/java/online/minipixel/smp/GambleSystem/Gamble_Nether.java
@@ -1,4 +1,115 @@
 package online.minipixel.smp.GambleSystem;
 
+import online.minipixel.smp.Main;
+import online.minipixel.smp.MessageAPI.Prefix;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Gamble_Nether {
+
+    List<Inventory> invs = new ArrayList<Inventory>();
+    public static ItemStack[] contents;
+    private int ItemIndex = 0;
+
+    public void gamble(Player player) {
+
+        ItemStack fee = new ItemStack(Material.DIAMOND, 12);
+        if (player.getInventory().getItemInMainHand().equals(fee)) {
+            player.getInventory().removeItem(fee);
+            this.spin(player);
+
+        } else {
+            player.closeInventory();
+            player.sendMessage(Prefix.Plugin_Prefix + "§cDu benötigst 12 Diamanten!");
+        }
+
+
+
+    }
+
+
+    public void shuffle (Inventory inv){
+        if (contents == null) {
+            ItemStack[] items = new ItemStack[21];
+            items[0] = new ItemStack(Material.OAK_WOOD, 32);
+
+
+            contents = items;
+        }
+
+        int statingIndex = ThreadLocalRandom.current().nextInt(contents.length);
+
+        for (int index = 0; index < statingIndex; index++) {
+            for (int itemstacks = 9; itemstacks < 18; itemstacks++) {
+                inv.setItem(itemstacks, contents[(itemstacks + ItemIndex) % contents.length]);
+            }
+            ItemIndex++;
+        }
+
+        ItemStack item = new ItemStack(Material.HOPPER);
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName("§8|");
+        item.setItemMeta(meta);
+        inv.setItem(4, item);
+
+    }
+
+    public void spin ( final Player player){
+        Inventory inv = Bukkit.createInventory(null, 27, "§8Nether");
+        shuffle(inv);
+        invs.add(inv);
+        player.openInventory(inv);
+
+        Random r = new Random();
+        double seconds = 10.0 + (15.0 - 10.0) * r.nextDouble();
+
+        new BukkitRunnable() {
+            double delay = 0;
+            int ticks = 0;
+            boolean done = false;
+
+            public void run() {
+                if (done)
+                    return;
+                ticks++;
+                delay += 1 / (20 * seconds);
+                if (ticks > delay * 10) {
+                    ticks = 0;
+
+                    for (int itemstacks = 9; itemstacks < 18; itemstacks++)
+                        inv.setItem(itemstacks, contents[(itemstacks + ItemIndex) % contents.length]);
+
+                    ItemIndex++;
+
+                    if (delay >= .5) {
+                        done = true;
+                        new BukkitRunnable() {
+                            public void run() {
+                                ItemStack item = inv.getItem(13);
+                                player.getInventory().addItem(item);
+                                player.playSound(player.getLocation(), Sound.BLOCK_BEACON_ACTIVATE, 10, 1);
+                                player.updateInventory();
+                                player.closeInventory();
+                                cancel();
+                            }
+                        }.runTaskLater(Main.getPlugin(Main.class), 50);
+                        cancel();
+                    }
+                }
+            }
+
+
+        }.runTaskTimer(Main.getINSTANCE(), 0, 1);
+    }
 }
