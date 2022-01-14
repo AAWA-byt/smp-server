9cb27c10be2081af78d1b9e2cb32e356f62c5761
AAWA-byt <mail@aaron.is>
2022-01-14 19:22:17 +0100

    :zap: changed gamble items and background

--- src/main/java/online/minipixel/smp/Inventorys/Gamble.java
+++ src/main/java/online/minipixel/smp/Inventorys/Gamble.java
@@ -13,26 +13,17 @@ public class Gamble {
 
     private static void create_gamble_menu() {
 
-        Main.getGamble().setItem(10, new ItemBuilder(Material.DIAMOND, 1).setDisplayname("§6§lLotterie 1 §7(Linksklick)")
-                .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).setLore("", "§7Preis: 1x Diamant", "").setLocalizedName("l1").build());
+        Main.getGamble().setItem(10, new ItemBuilder(Material.GRASS_BLOCK, 1).setDisplayname("§a§lOverworld §7(Linksklick)")
+                .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).setLore("", "§7Preis: 8x Diamant", "§cDu musst den benötigten Preis", "§cin der Hand halten, wenn", "§cdu die Lotterie öffnest!", "").setLocalizedName("l1").build());
 
-        Main.getGamble().setItem(11, new ItemBuilder(Material.DIAMOND, 1).setDisplayname("§6§lLotterie 2 §7(Linksklick)")
-                .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).setLore("", "§7Preis: 4x Diamant", "").setLocalizedName("l2").build());
+        Main.getGamble().setItem(12, new ItemBuilder(Material.NETHERRACK, 1).setDisplayname("§4§lNether §7(Linksklick)")
+                .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).setLore("", "§7Preis: 12x Diamant", "§cDu musst den benötigten Preis", "§cin der Hand halten, wenn", "§cdu die Lotterie öffnest!", "").setLocalizedName("l1").build());
 
-        Main.getGamble().setItem(12, new ItemBuilder(Material.DIAMOND, 1).setDisplayname("§6§lLotterie 3 §7(Linksklick)")
-                .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).setLore("", "§7Preis: 6x Diamant", "").setLocalizedName("l3").build());
+        Main.getGamble().setItem(14, new ItemBuilder(Material.END_STONE, 1).setDisplayname("§f§lTools §7(Linksklick)")
+                .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).setLore("", "§7Preis: 20x Diamant", "§cDu musst den benötigten Preis", "§cin der Hand halten, wenn", "§cdu die Lotterie öffnest!", "").setLocalizedName("l1").build());
 
-        Main.getGamble().setItem(13, new ItemBuilder(Material.BOOK, 1).setDisplayname("§9§lAnleitung")
-                .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).build());
-
-        Main.getGamble().setItem(14, new ItemBuilder(Material.DIAMOND, 1).setDisplayname("§6§lLotterie 4 §7(Linksklick)")
-                .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).setLore("", "§7Preis: 8x Diamant", "").setLocalizedName("l4").build());
-
-        Main.getGamble().setItem(15, new ItemBuilder(Material.DIAMOND, 1).setDisplayname("§6§lLotterie 5 §7(Linksklick)")
-                .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).setLore("", "§7Preis: 10x Diamant", "").setLocalizedName("l5").build());
-
-        Main.getGamble().setItem(16, new ItemBuilder(Material.DIAMOND, 1).setDisplayname("§6§lLotterie 6 §7(Linksklick)")
-                .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).setLore("", "§7Preis: 15x Diamant", "").setLocalizedName("l6").build());
+        Main.getGamble().setItem(16, new ItemBuilder(Material.IRON_PICKAXE, 1).setDisplayname("§9§lTools §7(Linksklick)")
+                .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).setLore("", "§7Preis: 25x Diamant", "§cDu musst den benötigten Preis", "§cin der Hand halten, wenn", "§cdu die Lotterie öffnest!", "").setLocalizedName("l1").build());
 
         Main.getGamble().setItem(26, new ItemBuilder(Material.BARRIER, 1).setDisplayname("§c§lSchließen §7(Linksklick)")
                 .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).setLocalizedName("close").build());
@@ -59,6 +50,12 @@ public class Gamble {
         Main.getGamble().setItem(8, new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE, 1).setDisplayname("§c")
                 .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).build());
         Main.getGamble().setItem(9, new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE, 1).setDisplayname("§c")
+                .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).build());
+        Main.getGamble().setItem(11, new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE, 1).setDisplayname("§c")
+                .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).build());
+        Main.getGamble().setItem(13, new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE, 1).setDisplayname("§c")
+                .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).build());
+        Main.getGamble().setItem(15, new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE, 1).setDisplayname("§c")
                 .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).build());
         Main.getGamble().setItem(17, new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE, 1).setDisplayname("§c")
                 .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).build());
