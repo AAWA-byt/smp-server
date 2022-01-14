4806bc98f9511ecd6b1ab913d95dc42efaaf1747
AAWA-byt <mail@aaron.is>
2022-01-14 23:39:03 +0100

    :zap: changed prefix from & to §

--- src/main/java/online/minipixel/smp/MessageAPI/Prefix.java
+++ src/main/java/online/minipixel/smp/MessageAPI/Prefix.java
@@ -3,7 +3,7 @@ package online.minipixel.smp.MessageAPI;
 public class Prefix {
 
     public static String System_Prefix = "§4System §8§l>> §f";
-    public static String WarpSystem_Prefix = "&x&f&f&0&0&0&0M&x&e&f&0&0&0&2i&x&d&f&0&1&0&3n&x&c&f&0&1&0&5i&x&c&0&0&2&0&7p&x&b&0&0&2&0&8i&x&a&0&0&2&0&ax&x&9&0&0&3&0&be&x&8&0&0&3&0&dl &8&l| &7";
+    public static String WarpSystem_Prefix = "§x§f§f§0§0§0§0M§x§e§f§0§0§0§2i§x§d§f§0§1§0§3n§x§c§f§0§1§0§5i§x§c§0§0§2§0§7p§x§b§0§0§2§0§8i§x§a§0§0§2§0§ax§x§9§0§0§3§0§be§x§8§0§0§3§0§dl §8§l| §7";
     public static String DiscordBot_Prefix = "§9Discord §8§l>> §f";
-    public static String Plugin_Prefix = "&x&f&f&0&0&0&0M&x&e&f&0&0&0&2i&x&d&f&0&1&0&3n&x&c&f&0&1&0&5i&x&c&0&0&2&0&7p&x&b&0&0&2&0&8i&x&a&0&0&2&0&ax&x&9&0&0&3&0&be&x&8&0&0&3&0&dl &8&l| &7";
+    public static String Plugin_Prefix = "§x§f§f§0§0§0§0M§x§e§f§0§0§0§2i§x§d§f§0§1§0§3n§x§c§f§0§1§0§5i§x§c§0§0§2§0§7p§x§b§0§0§2§0§8i§x§a§0§0§2§0§ax§x§9§0§0§3§0§be§x§8§0§0§3§0§dl §8§l| §7";
 }
