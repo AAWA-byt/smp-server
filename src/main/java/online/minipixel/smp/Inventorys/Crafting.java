3eb4781400c8b320edfc94318f98f3bc0260b5c5
AAWA-byt <mail@aaron.is>
2022-01-16 13:22:50 +0100

    :zap: added inventory manager

--- src/main/java/online/minipixel/smp/Inventorys/Crafting.java
+++ src/main/java/online/minipixel/smp/Inventorys/Crafting.java
@@ -2,4 +2,12 @@ package online.minipixel.smp.Inventorys;
 
 public class Crafting {
 
+    public static void open_crafting_menu() {
+
+    }
+
+    private static void create_crafting_menu() {
+
+    }
+
 }
