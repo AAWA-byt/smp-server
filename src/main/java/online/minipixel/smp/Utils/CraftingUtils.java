820e85a6cab2d4fdc2c70a1fd93cf1db0540c802
AAWA-byt <mail@aaron.is>
2022-01-17 12:21:52 +0100

    :sparkles: created utils_emerald_helmet method

--- src/main/java/online/minipixel/smp/Utils/CraftingUtils.java
+++ src/main/java/online/minipixel/smp/Utils/CraftingUtils.java
@@ -1,5 +1,24 @@
 package online.minipixel.smp.Utils;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
+
+
 public class CraftingUtils {
 
+    public void utils_emerald_helmet(Player player) {
+        Inventory ueh;
+        ueh = Bukkit.createInventory(null, 9*1, "§8Benötige Ressourcen für Emeraldhelm");
+
+
+        ueh.setItem(8, new ItemBuilder(Material.BARRIER, 1).setDisplayname("§c§lSchließen §7(Linksklick")
+                .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).build());
+
+        ueh.setItem(0, new ItemBuilder(Material.EMERALD, 5).build());
+
+        player.openInventory(ueh);
+    }
 }
