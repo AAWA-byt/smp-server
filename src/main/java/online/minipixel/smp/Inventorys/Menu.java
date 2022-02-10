c6443262432aa6ed44312463ffff0d39c96a3469
AAWA-byt <mail@aaron.is>
2022-02-10 15:49:59 +0100

    :zap: added new TODO

--- src/main/java/online/minipixel/smp/Inventorys/Menu.java
+++ src/main/java/online/minipixel/smp/Inventorys/Menu.java
@@ -20,7 +20,7 @@ public class Menu {
         Main.getMenu().setItem(13, new ItemBuilder(Material.DIAMOND, 1).setDisplayname("§6§lLotterie §7(Linksklick)")
                 .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).setLocalizedName("l2").build());
 
-
+        //TODO: add new feature soon
 
         Main.getMenu().setItem(26, new ItemBuilder(Material.BARRIER,1).setDisplayname("§c§lSchließen §7(Linksklick)")
                 .addItemFlags(ItemFlag.HIDE_ATTRIBUTES).setLocalizedName("l4").build());
