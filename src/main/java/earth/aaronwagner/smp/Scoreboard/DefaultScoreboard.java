89b4f6f068a090b7bab5289b6aaef1e05f819523
AAWA-byt <mail@aaronwagner.earth>
2022-07-08 01:27:43 +0200

    :zap: changed website

--- src/main/java/earth/aaronwagner/smp/Scoreboard/DefaultScoreboard.java
+++ src/main/java/earth/aaronwagner/smp/Scoreboard/DefaultScoreboard.java
@@ -26,7 +26,7 @@ public class DefaultScoreboard extends ScoreboardBuilder {
         player_on_network = PlaceholderAPI.setPlaceholders(player, player_on_network);
         player_play_time = PlaceholderAPI.setPlaceholders(player, player_play_time);
 
-        setScore("§chttps://github.com/AAWA-byt", 0);
+        setScore("§chttps://aaronwagner.earth", 0);
         setScore("§d", 1);
         setScore("§7Tode: §c" + player.getStatistic(Statistic.DEATHS), 2);
         setScore("§7Kills: §c" + player.getStatistic(Statistic.PLAYER_KILLS), 3);
