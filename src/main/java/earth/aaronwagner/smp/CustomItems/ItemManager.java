2fe0de0cc09adeabd5b7078a63a40ec3a186e8c8
AAWA-byt <mail@aaronwagner.earth>
2022-07-08 01:31:55 +0200

    :sparkles: created ItemManager.java

---
+++ src/main/java/earth/aaronwagner/smp/CustomItems/ItemManager.java
@@ -0,0 +1,23 @@
+package earth.aaronwagner.smp.CustomItems;
+
+
+import earth.aaronwagner.smp.CustomItems.Items.*;
+
+public class ItemManager {
+
+    public static void initItems() {
+
+        EmeraldHelmet.createEmeraldHelmet();
+        EmeraldChestplatte.getCustomEmeraldChestplate();
+        EmeraldLeggings.getCustomEmeraldleggings();
+        EmeraldBoots.getCustomEmeraldBoots();
+
+        EmeraldSword.getCustomEmeraldsword();
+        ObsidianSword.getCustomObsidianSword();
+        Lapispickaxe.getCustomLapisPickAxe();
+        Redstoneboots.getCustomRedStoneBoots();
+        Knockbackstick.getCustomKBStick();
+        EmeraldPickaxe.getCustomEmeraldPickAxe();
+
+    }
+}
